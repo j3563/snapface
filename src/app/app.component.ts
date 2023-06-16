9185bc265bbb137595a11cb97649e3c0f68f59cb
j3563 <136433527+j3563@users.noreply.github.com>
2023-06-16 10:48:56 +0200

    fin de tuto et deploiement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']}
 )
-export class AppComponent implements OnInit {
- 
-  faceSnaps!: FaceSnap[];
+export class AppComponent implements OnInit { 
   
-  ngOnInit() {
-    this.faceSnaps = [{
-      title: 'CANARD',
-      description: 'Ceci est un canard',
-      imageUrl: 'https://th.bing.com/th/id/OIP.xhhPcS2KleB8cFnl17NdpgHaFc?pid=ImgDet&rs=1',
-      createdDate: new Date(),
-      snaps: 2635
-    },
-    {
-      title: 'SURICATE',
-      description: 'Ceci est probablement un suricate',
-      imageUrl: 'https://th.bing.com/th/id/OIP.wK99jo47ZrPSyOo_QL0tagHaHa?w=192&h=193&c=7&r=0&o=5&pid=1.7',
-      createdDate: new Date(),
-      snaps: 98
-    },
-    {
-      title: 'SAINT FLORIS',
-      description: 'Sans doute le meilleur village',
-      imageUrl: 'https://www.communes.com/images/orig/nord-pas-de-calais/pas-de-calais/saint-floris_62350/saint-floris_191705.jpg',
-      createdDate: new Date(),
-      snaps: 99999,
-      location: 'Saint-Floris'
-    },
-  ];
-  }
+  ngOnInit() {}
 }
---
+++ src/app/services/face-snaps.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { FaceSnap } from '../models/face-snap.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FaceSnapsService {
+    faceSnaps: FaceSnap[] = [
+     {
+        id: 1,
+        title: 'CANARD',
+        description: 'Ceci est un canard',
+        imageUrl: 'https://th.bing.com/th/id/OIP.xhhPcS2KleB8cFnl17NdpgHaFc?pid=ImgDet&rs=1',
+        createdDate: new Date(),
+        snaps: 2635
+      },
+      {
+        id: 2,
+        title: 'SURICATE',
+        description: 'Ceci est probablement un suricate',
+        imageUrl: 'https://th.bing.com/th/id/OIP.mymANAZJvaU0x3Swqe0PiwHaE3?w=290&h=190&c=7&r=0&o=5&pid=1.7',
+        createdDate: new Date(),
+        snaps: 98
+      },
+      {
+        id: 3,
+        title: 'BANANE',
+        description: 'Je crois bien que cette chose est une banane',
+        imageUrl: "https://th.bing.com/th/id/R.8360829a408d8c8005f0d576a1ee3740?rik=p2xmB72HaOE7rw&pid=ImgRaw&r=0",
+        createdDate: new Date(),
+        snaps: 9999,
+        location: 'Saint-Floris'
+      },
+      {
+       id: 4,
+       title: 'FROMAGE',
+       description: 'Un très bon fromage',
+       imageUrl: "https://www.fromages-du-mezard.com/wp-content/uploads/2020/07/fromages-du-mezard-fromage.jpg",
+       createdDate: new Date(),
+       snaps: 231,
+       location: 'Aussois'
+      },
+      {
+       id: 5,
+       title: 'Soirée disco',
+       description: 'La folie chez Boris',
+       imageUrl: "https://th.bing.com/th/id/OIP.wxm80_2Cpv_8cwFg3BdDSAHaD4?w=305&h=180&c=7&r=0&o=5&pid=1.7",
+       createdDate: new Date(),
+       snaps: 628,
+      }
+    ];  
+   
+    getAllFaceSnaps(): FaceSnap[] {
+      return this.faceSnaps;
+    }
+
+    getFaceSnapById(faceSnapId: number): FaceSnap {
+      const faceSnap = this.faceSnaps.find(faceSnap => faceSnap.id === faceSnapId);
+      if (!faceSnap) {
+        throw new Error('FaceSnap not found');
+      } else {
+        return faceSnap;
+      }
+    }
+    
+    snapFaceSnapById(faceSnapId: number, snapType: 'snap' | 'unsnap'): void {
+      const faceSnap = this.getFaceSnapById(faceSnapId);
+      snapType === 'snap' ? faceSnap.snaps++ : faceSnap.snaps--;
+    }
+  }
